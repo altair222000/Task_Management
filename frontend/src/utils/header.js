1b8a8a533ce29cab73d2016c36882bf9a646b203
altair222000 <altair222000@gmail.com>
2026-09-24 16:45:25 -0600

    Delete frontend directory

--- frontend/src/utils/header.js
+++
@@ -1,9 +0,0 @@
-const getHeader = () => {
-	const token = localStorage.getItem("token");
-	return {
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${token}`,
-	};
-};
-
-export default getHeader;
